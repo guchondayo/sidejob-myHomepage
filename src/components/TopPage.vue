033b53bda27185e5648f45bd23a263a09a22c345
“guchondayo” <uto.to.be.engineer@gmail.com>
2023-01-16 08:35:56 +0900

    classで配列を使ったらバインドできるかどうか検証してみた

--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -1,25 +1,24 @@
 <template>
-    <div class="top-image">    
+    <div :class="imgSrc">  
+         
     </div>
+
 </template>
 <script>
     export default{
         data:function(){
             return{
+                imgSrc:["img-container"]
             }
         },
         mounted:function(){
             let count = 0
             let myImage = ["pic1.jpg", "pic2.jpg", "pic3.jpg"]
             setInterval(function(){
-                console.log("ループ前の数字",count)
-                console.log("image.length",myImage.length)
                 if(count >= myImage.length){
                     count = 0;
                 }
-                console.log(myImage[count])
                 count++;
-                console.log("ループ後の数字",count)
             },2000)
                 // https://mebee.info/2021/04/08/post-32646/
                 // setintervalにthisを入れた時の入れた時のスコープ
@@ -27,10 +26,19 @@
     }
 </script>
 <style>
+    .img-container{
+        height:400px;
+        width:100%;
+        background-color: blue;
+        margin: 0;
+    }
     .top-image{
-        background-image: url(../../public/pic1.jpg);
-        background-size:cover ;
-        min-height: 500px;
+        /* background-image: url(../../public/pic1.jpg); */
+        /* background-size:cover ; */
+        object-fit: cover;
+        width: 100%;
+        height: auto;
+        
         margin: 0;
     }
     /* https://jajaaan.co.jp/web-production/frontend/css-background/ */
