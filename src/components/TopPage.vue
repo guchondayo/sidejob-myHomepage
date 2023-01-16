1eb3baffe5cc70c5dfe2b969ea46fa7728ca2205
“guchondayo” <uto.to.be.engineer@gmail.com>
2023-01-16 11:41:04 +0900

    とりあえず、TRUEFALSEはできてよかった

--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="imgSrc">  
+    <div :class="{test: isTest}">  
          
     </div>
 
@@ -8,7 +8,7 @@
     export default{
         data:function(){
             return{
-                imgSrc:["img-container"]
+                isTest:true
             }
         },
         mounted:function(){
@@ -22,24 +22,30 @@
             },2000)
                 // https://mebee.info/2021/04/08/post-32646/
                 // setintervalにthisを入れた時の入れた時のスコープ
+                // img srcを使うと、、
+                // https://ikatakos.com/pot/programming/web_development/javascript/vuejs/tips/img
+
+                // https://xminatolog.com/post/773
         }
     }
 </script>
 <style>
+    .test{
+        background-image: url(../../public/pic2.jpg);
+        background-size:cover;
+        width: 100%;
+        height: 400px;
+    }
     .img-container{
         height:400px;
         width:100%;
         background-color: blue;
         margin: 0;
     }
-    .top-image{
-        /* background-image: url(../../public/pic1.jpg); */
-        /* background-size:cover ; */
-        object-fit: cover;
-        width: 100%;
-        height: auto;
-        
-        margin: 0;
-    }
     /* https://jajaaan.co.jp/web-production/frontend/css-background/ */
+    /* v-bindを紐づける */
+    /* https://johobase.com/vue-js-v-bind-directive-class/ */
+    /* とりあえず */
+    /* 1.img srcを使って切り替えることは可能 */
+    /* 2.v-bindを使っても複数のclassを導入することは可能 */
 </style>
