d6c7b8e9804fbd8010927c39ad174b0e0b258b16
“guchondayo” <uto.to.be.engineer@gmail.com>
2023-01-20 16:35:23 +0900

    フッター完成

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,7 @@
 <template>
     <footer>
-        <ul>
+        <p>サイトマップ</p>
+        <ul class="site-map">
             <li v-for="(value, key) in name" :key="key">{{ value }}</li>
         </ul>
         <ul>
@@ -21,11 +22,20 @@
 
 <style scoped>
     footer{
-     height: 300px;
-     background-color: black;
+        height: 300px;
+        background-color: black;
 
+    }
+    footer p{
+        padding: 2%;
+        text-align: center;
+        color:white;
     }
     footer ul li{
         color: aliceblue;
+    }
+    .site-map{
+        width:30%;
+        padding-left: 50%;
     }
 </style>
